d956be4938fc8c39d21a4f82df9993c60d29fd34
Abdullah Tariq <66561984+AbdullahT-BK@users.noreply.github.com>
2022-06-19 23:59:25 -0400

    Create blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,15 @@
+import random
+
+# deck of cards / player dealer hand
+deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,
+       'J','Q',K','A','J','Q',K','A','J','Q',K','A','J','Q',K','A']
+playerand = []
+dealerHand = []
+
+# deal the cards
+
+# calculate the total of each hand
+
+# check for winner
+
+# game loop
